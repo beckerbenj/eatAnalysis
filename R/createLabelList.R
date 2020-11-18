11d73591ffbde8289e0b8e9d02ef9c5405be9847
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2020-11-19 00:10:14 +0100

    0.3.3
    change createLabelList

--- R/createLabelList.R
+++ R/createLabelList.R
@@ -9,6 +9,7 @@
 #' variable and value labels.
 #'
 #'@param dfr A data.frame with variable and value labels stored as attributes using the convention of \code{\link{convertLabel}}.
+#'@param additionalAttributes Optional: Vector of names of additional attributes which should be collected in the returned data frame.
 #'
 #'@return A \code{data.frame}.
 #'
@@ -20,24 +21,27 @@
 #'dat  <- convertLabel(dat)
 #'atts <- createLabelList(dat)
 #'@export
-createLabelList <- function ( dfr ) {
-stopifnot ( class(dfr) == "data.frame")
-varList<- do.call(plyr::rbind.fill, lapply(colnames(dfr), FUN = function ( v ) {
-  lbs  <- attributes(dfr[,v])
-  if (!is.null(lbs[["varLabel"]]))  {
-    varLab <- unlist(lbs[["varLabel"]])
-  }  else  {
-    varLab <- NA
-  }
-  if(!is.null(lbs[["valLabel"]]) && length(lbs[["valLabel"]])>0)  {
-    vals <- data.frame ( value = as.vector(unlist(lbs[["valLabel"]])), valLabel = names(lbs[["valLabel"]]), stringsAsFactors = FALSE)
-    ret <- data.frame ( varName = v, class = class(dfr[,v]), varLabel = varLab , vals[,c("value", "valLabel")], stringsAsFactors = FALSE)
-  }  else  {
-    ret <- data.frame ( varName = v, class = class(dfr[,v]), varLabel = varLab , stringsAsFactors = FALSE)
-  }
-  return(ret)}))
-return(varList)
-}
-
+createLabelList <- function ( dfr, additionalAttributes = NULL ) {
+         stopifnot ( class(dfr) == "data.frame")
+         varList<- do.call(plyr::rbind.fill, lapply(colnames(dfr), FUN = function ( v ) {
+                   lbs  <- attributes(dfr[,v])
+                   if (!is.null(lbs[["varLabel"]]))  {
+                        varLab <- unlist(lbs[["varLabel"]])
+                   }  else  {
+                        varLab <- NA
+                   }
+                   if(!is.null(lbs[["valLabel"]]) && length(lbs[["valLabel"]])>0)  {
+                        vals <- data.frame ( value = as.vector(unlist(lbs[["valLabel"]])), valLabel = names(lbs[["valLabel"]]), stringsAsFactors = FALSE)
+                        ret <- data.frame ( varName = v, class = class(dfr[,v]), varLabel = varLab , vals[,c("value", "valLabel")], stringsAsFactors = FALSE)
+                   }  else  {
+                        ret <- data.frame ( varName = v, class = class(dfr[,v]), varLabel = varLab , stringsAsFactors = FALSE)
+                   }
+                   if (!is.null(additionalAttributes)) {
+                        for ( i in additionalAttributes) {
+                              ret[,i] <- lbs[[i]]
+                        }
+                   }
+                   return(ret)}))
+         return(varList)}
 
 
