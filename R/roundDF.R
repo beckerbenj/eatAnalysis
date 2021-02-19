db5ad40dd181ca51d1aaaae72b0929cfac17d70d
Benjamin Becker <benj.becker@web.de>
2021-02-19 20:30:05 +0100

    roundDF

---
+++ R/roundDF.R
@@ -0,0 +1,22 @@
+#############################################################################
+#' Round data frame.
+#'
+#' Round all numeric variables in a \code{data.frame}.
+#'
+#'@param df A \code{data.frame}.
+#'@param roundN Integer indicating the number of decimal places.
+#'
+#'@return Returns the rounded \code{data.frame}.
+#'
+#'@examples
+#'roundDF(mtcars, roundN = 0)
+roundDF <- function(df, roundN = 3) {
+  roundCol <- function(var, roundN) {
+    if(!is.numeric(var)) return(var)
+    round(var, digits = roundN)
+  }
+
+  out <- as.data.frame(lapply(df, roundCol, roundN = roundN), stringsAsFactors = FALSE)
+  names(out) <- names(df)
+  out
+}
