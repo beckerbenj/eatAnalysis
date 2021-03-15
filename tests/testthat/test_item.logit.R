5496e1bf053638bbff8eed296267a0fa5f782494
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2021-03-15 13:19:24 +0100

    modify test for item.logit()

--- tests/testthat/test_item.logit.R
+++ tests/testthat/test_item.logit.R
@@ -2,7 +2,7 @@ test_that("item.logit", {
   z <- rnorm(100, 0, 2)
   d <- item.logit(z = z, slope = 4, thr = 0)
   expect_true(all(d$x %in% 0:1))
-  expect_true(mean(d$x) > 0.4)
-  expect_true(mean(d$x) < 0.6)
+  expect_true(mean(d$x) > 0.25)
+  expect_true(mean(d$x) < 0.75)
 })
 
