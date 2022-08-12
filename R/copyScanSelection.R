7ef0b040422677267bd2dac38bf1bc45eb0b3d41
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2022-08-12 18:09:11 +0200

    copyScanSelection with selected codes

--- R/copyScanSelection.R
+++ R/copyScanSelection.R
@@ -2,10 +2,12 @@
 #############################################################################
 #' Copy booklet scan selection to desired directory
 #'
-#' Functions allows to copy the scans of a selection of variables,
-#' sort by variable codes, into a desired directory.
+#' Functions allows to copy the scans of a subset of variables,
+#' sort by variable codes, into a desired directory. This might be useful, if,
+#' for example, all scans where variable \code{"D122a"} was scored with 1 (correct)
+#' should be collected in a common folder.
 #'
-#'@param vars character string of variables whose scans should be copied.
+#'@param vars character string of variables whose scans should be copied. If missing, all available variables will be used.
 #'@param dat wide format data which contain these variables. Names in \code{vars} must match column names of \code{dat}.
 #'@param id column number or column name of the person identifier variable in the wide format data.
 #'@param sourceDir The directory which contains the scans.
@@ -19,9 +21,12 @@
 #'@param varColumn character string of the variable identifier column name in the codebook.
 #'@param bookletColumnPrefix Character string of the booklet identifier prefix in the codebook. The codebook
 #'usually contains several booklet columns which should begin with a common identifier.
+#'@param exclude Character string of codes which should be ignored for selection. If all codes should
+#'be used, type \code{exclude = ""} or \code{exclude = NULL}.
 #'@param separators Character strings which separates booklet identifier and person identifier in the
 #'filenames of the scans.
-#'@param suffix Suffix of the filenames of the scans.
+#'@param suffix Suffix of the filenames of the scans. If the scan files have multiple suffixes, you
+#'can use \code{suffix = ".TIF|.tif"}, fo example.
 #'
 #'@return No return, the files will be written on disk.
 #'
@@ -50,45 +55,56 @@
 #'@export
 ### foo <- copyScanSelection(codebook, vars=unlist(variablen), dat=dat, id=1, sourceDir = "s:/Vera3-Scans/Deutsch/V3_Pilot_2015/Depot_100", targetDir = "N:/archiv/test")
 ### prefixPattern = "TH[:digit:]{2}-[:digit:]{2}_[:digit:]{10}"
-copyScanSelection <- function ( vars, dat, id, sourceDir, targetDir, codebook, startRow = 4, sheet = "Codebook", varColumn = "Variable", bookletColumnPrefix = "TH", separators = c("-", "_"), suffix = ".TIF") {
-  if(length(id) != 1 ) {stop("Argument 'id' must be of length 1.\n",sep="")}
-  if(class(vars) != "character") {stop("Argument 'vars' must be of class 'character'.\n",sep="")}
-  if(length(vars) != length(unique(vars))) {stop("'vars' is not unique.\n")}
-  allV <- list(ID = id, variablen=vars )
-  allN <- lapply(allV, FUN=function(ii) {eatTools::existsBackgroundVariables(dat = dat, variable=ii)})
-  if (is.character(codebook)) {codebook <- data.frame ( readxl::read_excel(codebook, sheet = "Codebook", skip = startRow-1, col_types = "text"), stringsAsFactors = FALSE) }
-  liste<- do.call("rbind", lapply (allN[["variablen"]], FUN = function (va) {
-    codes <- setdiff(names(table(dat[,va])), c("mbd", "mnr", "mci", "mnr", "mir", "mbi", "9", "97", "98", "99", "7","8"))
-    sepCod<- do.call("rbind", lapply(codes, FUN = function ( co ) {
-      id <- dat[which(dat[,va] == co),allN[["ID"]] ]                  ### alle IDs raussuchen, die diesen code haben
-      rw <- grep(va, codebook[,varColumn])                            ### in welchen testheften (Zeilen) kommt die variable "va" vor?
-      if(length(rw) == 0) {stop(paste0("Cannot find variable '",va,"' in the codebook.\n"))}
-      if ( length(setdiff(unique(codebook[rw,"Variable"]), "")) > 1) {
-        cat(paste0("Item '",va,"' seems to be aggregated from '",paste(setdiff(unique(codebook[rw,"Variable"]), ""), collapse= "', '"),"'. \nScan selection is skipped as aggregated scores do not match variable raw scores.\n"))
-        return(NULL)}
-      th <- codebook[rw[1], grep(paste0("^", bookletColumnPrefix), colnames(codebook), value=TRUE)]
-      if(all(is.na(th))) {
-        cat(paste0("Variable '",va,"': codebook does not contain any valid information about booklet occurrences. All '",bookletColumnPrefix,"'-columns seem to be empty. Skip '",va,"'.\n"))
-        return(NULL)}
-      th <- th[which(!is.na(th))]
-      str<- paste0(names(th), separators[1], unlist(th), separators[2])
-      str<- expand.grid(str, id)
-      str<- paste0(str[,1], str[,2])
-      str<- paste0(str, suffix)
-      dfr<- data.frame ( variable = va, code = co, scans = str, stringsAsFactors = FALSE)
-      return(dfr)}))
-    return(sepCod)}))                                                   ### jetzt aus 'liste' alle scans loeschen, die es gar nicht gibt
-  scans<- list.files(path = sourceDir, pattern = paste0(suffix, "$"), recursive = TRUE)
-  if ( length(scans)==0) {stop(paste0("Cannot found any scan files in source directory '",sourceDir,"'.\n"))}
-  scan2<- eatTools::halveString(string = scans, pattern="/", first=FALSE)
-  if (length(intersect(liste[,"scans"], scan2[,2])) == 0) {warning("keine Scans im Verzeichnis gefunden.\n")}
-  weg  <- setdiff(liste[,"scans"], scan2[,2])
-  if ( length(weg)>0) { liste <- liste[-eatTools::whereAre(weg, liste[,"scans"], verbose=FALSE),]}
-  b    <- match(liste[,"scans"],scan2[,2])
-  liste[,"quelle"] <- file.path(sourceDir, scans[b])
-  if ( dir.exists(targetDir) == FALSE ) {dir.create(targetDir)}
-  foo  <- lapply(unique(liste[,"variable"]), FUN = function(y) {
-    dir.create(file.path(targetDir, y))                                 ### Verzeichnis fuer jede einzelne Variable erzeugen, und darunter: Unterverzeichnis fuer jeden einzelnen Code jeder einzelnen Variable erzeugen
-    lapply(unique(liste[which(liste[,"variable"] == y),"code"]), FUN = function(co) { dir.create(file.path(targetDir, y, co))}) })
-  foo2 <- file.copy(from = liste[,"quelle"], to = file.path(targetDir, liste[,"variable"], liste[,"code"], liste[,"scans"]))
-  return(foo2)}
+copyScanSelection <- function ( vars, dat, id, sourceDir, targetDir, codebook, startRow = 4, sheet = "Codebook", varColumn = "Variable", bookletColumnPrefix = "TH", exclude = c("mbd", "mnr", "mci", "mnr", "mir", "mbi", "9", "97", "98", "99", "7","8"), separators = c("-", "_"), suffix = ".TIF") {
+    if(length(id) != 1 ) {stop("Argument 'id' must be of length 1.\n",sep="")}
+    if(class(vars) != "character") {stop("Argument 'vars' must be of class 'character'.\n",sep="")}
+    if(length(vars) != length(unique(vars))) {stop("'vars' is not unique.\n")}
+    dat  <- eatTools::makeDataFrame(dat)
+    if (!all(vars %in% colnames(dat))) {
+        weg <- setdiff(vars, colnames(dat))
+        message(paste0("Following ",length(weg), " variable(s) which are missing in data will be ignored: '", paste(weg, collapse= "', '"),"'."))
+        vars<- intersect(vars, colnames(dat))
+        if (length(vars) == 0) {stop("No common variables in 'vars' and data.")}
+    }
+    allV <- list(ID = id, variablen=vars )
+    allN <- lapply(allV, FUN=function(ii) {eatTools::existsBackgroundVariables(dat = dat, variable=ii)})
+    if (is.character(codebook)) {codebook <- data.frame ( readxl::read_excel(codebook, sheet = "Codebook", skip = startRow-1, col_types = "text"), stringsAsFactors = FALSE) }
+    liste<- do.call("rbind", lapply (allN[["variablen"]], FUN = function (va) {
+            codes <- setdiff(unique(dat[,va]), unique(eatTools::crop(exclude)))
+            if ( length(codes) == 0) {
+                 message(paste0("Variable '",va,"': no valid codes remain. All codes of '",va,"' ('",paste(unique(dat[,va]), collapse="', '"),"') are a subset of codes captured in 'exclude'."))
+                 return(NULL)
+            }
+            sepCod<- do.call("rbind", lapply(codes, FUN = function ( co ) {
+                id <- dat[which(dat[,va] == co),allN[["ID"]] ]                  ### alle IDs raussuchen, die diesen code haben
+                rw <- grep(va, codebook[,varColumn])                            ### in welchen testheften (Zeilen) kommt die variable "va" vor?
+                if(length(rw) == 0) {stop(paste0("Cannot find variable '",va,"' in the codebook.\n"))}
+                if ( length(setdiff(unique(codebook[rw,"Variable"]), "")) > 1) {
+                   message(paste0("Item '",va,"' seems to be aggregated from '",paste(setdiff(unique(codebook[rw,"Variable"]), ""), collapse= "', '"),"'. \nScan selection is skipped as aggregated scores do not match variable raw scores."))
+                   return(NULL)}
+                th <- codebook[rw[1], grep(paste0("^", bookletColumnPrefix), colnames(codebook), value=TRUE)]
+                if(all(is.na(th))) {
+                   message(paste0("Variable '",va,"': codebook does not contain any valid information about booklet occurrences. All '",bookletColumnPrefix,"'-columns seem to be empty. Skip '",va,"'."))
+                   return(NULL)}
+                th <- th[which(!is.na(th))]
+                str<- paste0(names(th), separators[1], unlist(th), separators[2])
+                str<- expand.grid(str, id)
+                str<- paste0(str[,1], str[,2])
+                str<- paste0(str, suffix)
+                dfr<- data.frame ( variable = va, code = co, scans = str, stringsAsFactors = FALSE)
+                return(dfr)}))
+            return(sepCod)}))                                                   ### jetzt aus 'liste' alle scans loeschen, die es gar nicht gibt
+    scans<- list.files(path = sourceDir, pattern = paste0(suffix, "$"), recursive = TRUE)
+    if ( length(scans)==0) {stop(paste0("Cannot found any scan files in source directory '",sourceDir,"'.\nPlease check if the chosen suffix ('",suffix,"') is correct."))}
+    scan2<- eatTools::halveString(string = scans, pattern="/", first=FALSE)
+    if (length(intersect(tolower(liste[,"scans"]), tolower(scan2[,2]))) == 0) {warning("keine Scans im Verzeichnis gefunden.")}
+    weg  <- setdiff(tolower(liste[,"scans"]), tolower(scan2[,2]))
+    if ( length(weg)>0) { liste <- liste[-eatTools::whereAre(weg, tolower(liste[,"scans"]), verbose = FALSE),]}
+    b    <- match(tolower(liste[,"scans"]),tolower(scan2[,2]))
+    liste[,"quelle"] <- file.path(sourceDir, scans[b])
+    if ( dir.exists(targetDir) == FALSE ) {dir.create(targetDir, recursive=TRUE)}
+    foo  <- lapply(unique(liste[,"variable"]), FUN = function(y) {
+            dir.create(file.path(targetDir, y))                                 ### Verzeichnis fuer jede einzelne Variable erzeugen, und darunter: Unterverzeichnis fuer jeden einzelnen Code jeder einzelnen Variable erzeugen
+            lapply(unique(liste[which(liste[,"variable"] == y),"code"]), FUN = function(co) { dir.create(file.path(targetDir, y, co))}) })
+    foo2 <- file.copy(from = liste[,"quelle"], to = file.path(targetDir, liste[,"variable"], liste[,"code"], liste[,"scans"]))
+    return(foo2)}
